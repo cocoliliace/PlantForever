96aeb1b3ca0c5ce2a4d3cd342724c3554e76fab4
ChingChang9 <chingtheprogrammer@icloud.com>
2019-06-30 14:09:35 +0800

    default settings

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'About' }">About Us</router-link>
+      <router-link :to="{ name: 'TreeRegistration' }">Tree Registration</router-link>
+      <router-link :to="{ name: 'Volunteer' }">Volunteer</router-link>
+      <router-link :to="{ name: 'Contact' }">Contact</router-link>
+      <router-link :to="{ name: 'Donate' }">Donate</router-link>
     </div>
+
     <router-view/>
+
+    <footer>
+
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0px;
 }
 #nav {
-  padding: 30px;
   a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    padding: 10px 30px;
+    text-decoration: none;
+    color: black;
+    font-size: 25px;
+    &:hover {
+      background-color: green;
     }
   }
 }
--- src/router.js
+++ src/router.js
@@ -1,25 +1,88 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import About from "./views/Home.vue";
+import TreeRegistration from "./views/TreeRegistration.vue";
+import Volunteer from "./views/Volunteer.vue";
+import Contact from "./views/Contact.vue";
+import Donate from "./views/Donate.vue";
+import Error from "./views/Error.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+const router =  new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "Home",
+      component: Home,
+      meta: {
+        title: "PlantForever | Edmonton Nonprofit"
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: "/about",
+      name: "About",
+      component: About,
+      meta: {
+        title: "PlantForever | Edmonton Nonprofit"
+      }
+    },
+    {
+      path: "/tree-registration",
+      name: "TreeRegistration",
+      component: TreeRegistration,
+      meta: {
+        title: "PlantForever | Edmonton Nonprofit"
+      }
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      component: Contact,
+      meta: {
+        title: "PlantForever | Edmonton Nonprofit"
+      }
+    },
+    {
+      path: "/Volunteer",
+      name: "Volunteer",
+      component: Volunteer,
+      meta: {
+        title: "PlantForever | Edmonton Nonprofit"
+      }
+    },
+    {
+      path: "/donate",
+      name: "Donate",
+      component: Donate,
+      meta: {
+        title: "PlantForever | Edmonton Nonprofit"
+      }
+    },
+    {
+      path: "*",
+      name: "Error",
+      component: Error,
+      meta: {
+        title: "PlantForever | Edmonton Nonprofit"
+      }
+    },
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { x: 0, y: 0 };
     }
-  ]
-})
+  }
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || "PlantForever";
+  next();
+});
+
+export default router;

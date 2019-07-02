ae6ecb9029a58fb7df501355b1fb75a752dd74ee
Coco Liliace <coco@liliace.dev>
2019-07-02 08:19:36 +0800

    no source map

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,15 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "@/assets/variables.scss";`
+        data: '@import "@/assets/variables.scss";'
       }
     }
-  }
+  },
+
+  publicPath: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: false,
+  parallel: undefined
 };

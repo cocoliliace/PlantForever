ab66c1c592410e703d1cda89d64599c524e3747e
ChingChang9 <chingtheprogrammer@icloud.com>
2019-08-10 14:27:31 +0800

    updated content

--- src/views/About.vue
+++ src/views/About.vue
@@ -33,47 +33,57 @@
             century as a result of human activity.
           </div>
           <div class="secondary-text">
-            A large aspect of climate change is global warming. This occurs when man-made fossil
-            fuels, such as carbon dioxide, are emitted into the atmosphere, resulting in the
-            trapping of heat. The United Nations states that “quantities of greenhouse gases in the
-            atmosphere have risen to record levels not seen in three million years.” Since the late
-            19th century, the earth’s average surface temperature has risen about 0.9°C, as stated
-            by NASA.
+            A significant aspect of climate change is global warming. Global warming occurs when
+            fossil fuels are burned, emitting harmful gases, primarily CO2, into the atmosphere.
+            These greenhouse gases trap heat from escaping into space and contribute to the warming
+            of our planet. The United Nations states that “quantities of greenhouse gases in the
+            atmosphere have risen to record levels not seen in three million
+            years”<sup>[<a href="https://www.un.org/en/sections/issues-depth/climate-change/index.html" target="_blank">1</a>]</sup>.
+            Since the late 19th century, the earth’s average surface temperature has risen about
+            0.9°C!<sup>[<a href="https://climate.nasa.gov/evidence" target="_blank">2</a>]</sup>
           </div>
           <div class="secondary-text">
-            Higher global temperatures have detrimental effects on our planet. Just a few are listed
-            here: ocean acidification, the loss of our ice caps, rising sea levels, decrease in
-            biodiversity, loss of forests, and negative effects on human health and economy. To
-            prevent further irreversible impacts, immediate actions must take place.
+            Greater global temperatures have detrimental effects on our planet; ocean acidification,
+            ice cap depletion, and biodiversity reduction are few of the adverse impacts. Immediate
+            action must take place to prevent additional repercussions.
           </div>
           <div class="secondary-text">
             The Paris Agreement signed in 2016 aims to keep global temperature rise this century
-            below 2°C, in regards to levels in the pre-industrial era. As reports conducted by the
-            Intergovernmental Panel on Climate Change (IPCC) in 2018 found that many adverse effects
-            of climate change appear after a rise about 1.5°C.
+            below 2°C, in regards to levels in the pre-industrial
+            era.<sup>[<a href="https://unfccc.int/process-and-meetings/the-paris-agreement/the-paris-agreement" target="_blank">3</a>]</sup>
+            However, reports conducted by the Intergovernmental Panel on Climate Change (IPCC) in
+            2018 found that many irreversible effects of climate change appear after a rise of only
+            1.5°C.<sup>[<a href="https://unfccc.int/process-and-meetings/the-paris-agreement/the-paris-agreement" target="_blank">4</a>]</sup>
           </div>
           <div class="secondary-text">
             To keep levels below a 1.5°C rise, PlantForever takes a step forward as a leader for the
             billions of people and creatures impacted by climate change.
           </div>
+          <div class="secondary-text">
+            Learn what you can do <router-link :to="{ name: 'Error' }">here</router-link>!
+          </div>
         </div>
         <div class="column">
           <div class="primary-text">Spread Awareness</div>
           <div class="secondary-text">
-            The primary reason PlantForever plants in urban settings is to raise awareness.
+            The primary reason PlantForever plants in cities is to raise awareness.
           </div>
           <div class="secondary-text">
-            With an increase in urbanization, there is a larger disconnect between people and
-            nature; we tend to be trapped in our offices and homes and forget that the environment
-            exists. As a result, people are observing and considering the impacts of climate change
-            less and less.
+            With an increase in urbanization, there is a more substantial disconnect between people
+            and nature; the average person is trapped in their offices and homes and forgets that
+            the environment exists. As a result, people are considering the impacts of climate
+            change to a lesser extent.
           </div>
           <div class="secondary-text">
-            PlantForever works to eliminate this divide and create understands of the repercussions
-            of climate change. When we plant throughout the city and spread information, people take
-            time to fathom their impact on the planet and understand what they can do to help. This
-            also inspires people to take a leadership role and contribute. As we really are all in
-            this together, PlantForever recognizes that the best way to create change is to educate.
+            PlantForever strives to eliminate this divide and raise awareness of the consequences of
+            climate change. When PlantForever plants trees throughout the city, people take time to
+            fathom their impact on the planet and understand what they can do to help. Additionally,
+            through social media, PlantForever aims to encourage people to take a leadership role
+            and contribute. PlantForever recognizes that the best way to create change is to
+            educate.
+          </div>
+          <div class="secondary-text">
+            To view a list of urban specific benefits, click <router-link :to="{ name: 'Error' }">here</router-link>!
           </div>
         </div>
       </div>
@@ -93,6 +103,13 @@ export default {
   .columns {
     display: flex;
     justify-content: space-between;
+    a {
+      color: $green;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
     img {
       float: left;
       width: 500px;

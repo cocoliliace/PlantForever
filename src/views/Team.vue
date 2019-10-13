e1c901ac26b984caea424a1b60848b81347f3b7e
Coco Liliace <coco@liliace.dev>
2019-10-12 20:36:40 -0600

    Refracted css

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -32,14 +32,14 @@ export default {
         {
           name: "Marmik Patel",
           title: "Founding President",
-          photo: "nebelung",
+          photo: "marmik",
           story: "Marmik founded PlantForever in 2017 to create change. After learning about the limited time we have until the climate crisis poses irreversible global changes, Marmik took the initiative and began planting trees. Since then, Marmik has transformed a mere idea into a fully functioning organization; he assembled a team, launched city-wide tree planting initiatives, created local partnerships, and organized platforms to spread awareness. Marmik is proud of his city’s urban forest but knows it can expand. Now 17, Marmik aims to develop PlantForever internationally, spreading awareness and encouraging vast tree-canopy expansion in urban areas. He intends to inspire a movement empowering individuals to take action globally"
         },
         {
           name: "Pedro Almeida",
           title: "VP of Human Resources",
-          photo: "nebelung",
-          story: "Pedro, now 17 is looking forward to his final year before post-secondary school where he will focus on his schooling as well as assisting the executive team to expand PlantForever as far as possible, by advancing communication between members as well as with people outside of the organization. He assisted the team in large events as well as messages to organizations that has aided PlantForever for his time here."
+          photo: "pedro",
+          story: "Pedro, now 17, is looking forward to his final year before post-secondary school where he will focus on his schooling as well as assisting the executive team to expand PlantForever as far as possible, by advancing communication between members as well as with people outside of the organization. He assisted the team in large events as well as messages to organizations that has aided PlantForever for his time here"
         },
         {
           name: "Nebelung Meow",
@@ -51,27 +51,45 @@ export default {
           name: "Ching Chang",
           title: "VP of IT",
           photo: "ching",
-          story: "As a 17 years old environmentalist, Ching joined PlantForever with the incentive of combating climate change and urban deforestation. With a passion for web developing, Ching is looking forward to not only maximize the ways that people can reach out to PlantForever from everywhere in the world, but to also make the process as simple as possible"
+          story: "As a 17-year old environmentalist, Ching joined PlantForever to combat climate change and urban deforestation. With a passion for web developing, Ching is looking forward to not only maximize the ways that people can reach out to PlantForever from everywhere in the world, but to also facilitate the process with a simplistic design"
         },
         {
           name: "Fatima Shamas",
           title: "VP of Social Media",
-          photo: "nebelung",
+          photo: "fatima",
           story: "Fatima joined PlantForever in 2018 as a volunteer, but later became part of the team in hopes of spreading awareness through social media. Her goal remains the same: to educate and inform people about climate change and help them to make changes within their lifestyle to combat it"
         },
         {
           name: "Vanshaj Upadhyay",
           title: "VP of Merchandise",
-          photo: "nebelung",
+          photo: "logo",
           story: "At 16 years old, Vanshaj joined the PF team almost at the very beginning, when things were first starting to fall in place. He enjoys working at PF because he is interested in environmental matters and taking care of the planet. Vanshaj’s goal at PlantForever is to battle pollution and climate change through the plantation of trees. Vanshaj is dedicated to the PlantForever mission"
         },
         {
           name: "Eric Li",
           title: "Photographer",
-          photo: "nebelung",
+          photo: "logo",
           story: "Eric initially joined PlantForever as a volunteer, helping the team with small photoshop tasks. Having a hobby in taking pictures, he officially joined the PF team in mid 2019 as the official photographer. He now works with all team members, taking photos during planting events"
         }
       ]
+    }
+  },
+  mounted() {
+    this.animateCards();
+    window.addEventListener("resize", this.animateCards);
+    window.addEventListener("scroll", this.animateCards);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.animateCards);
+    window.removeEventListener("scroll", this.animateCards);
+  },
+  methods: {
+    animateCards() {
+      for (let index = 0; index < document.querySelectorAll(".card").length; index++) {
+        if (window.innerHeight > document.querySelectorAll(".card")[index].getClientRects()[0].top) {
+          document.querySelectorAll(".card")[index].classList.add("slide-up");
+        }
+      }
     }
   }
 }
@@ -91,6 +109,9 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+    transition-duration: 0.9s;
+    opacity: 0;
+    transform: translateY(100px);
     .name {
       margin: 12px;
       font-size: 25px;
@@ -109,18 +130,10 @@ export default {
       font-family: sans-serif;
       font-size: 17px;
     }
+    &.slide-up {
+      opacity: 1;
+      transform: translateY(0px);
+    }
   }
 }
 </style>
-
-
-
-
-
-
-
-
-
-
-
-

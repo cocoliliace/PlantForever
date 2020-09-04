4ca20141d6c804150d5e1793ecf99fd537ddcce3
ChingChang9 <chingtheprogrammer@gmail.com>
2020-09-05 02:51:37 +0800

    No more schubert chokecherry

--- src/views/TreeInPots.vue
+++ src/views/TreeInPots.vue
@@ -38,7 +38,7 @@
           <label><input v-model="preferredList[0]" class="checkbox" type="checkbox" /><span class="checkmark"></span>Black Cherry<span v-if="preferredList[0]"> X <input v-model="amountList[0]" class="amount" /></span></label>
           <label><input v-model="preferredList[1]" class="checkbox" type="checkbox" /><span class="checkmark"></span>Colorado Spruce<span v-if="preferredList[1]"> X <input v-model="amountList[1]" class="amount" /></span></label>
           <label><input v-model="preferredList[2]" class="checkbox" type="checkbox" /><span class="checkmark"></span>Red Maple<span v-if="preferredList[2]"> X <input v-model="amountList[2]" class="amount" /></span></label>
-          <label><input v-model="preferredList[3]" class="checkbox" type="checkbox" /><span class="checkmark"></span>Schubert Chokecherry<span v-if="preferredList[3]"> X <input v-model="amountList[3]" class="amount" /></span></label>
+          <label><input v-model="preferredList[3]" class="checkbox" type="checkbox" /><span class="checkmark"></span>Schubert Chokecherry<span v-if="preferredList[3]"> X <input v-model="amountList[3]" class="amount" /></span> (No more remaining for 2020 planting season)</label>
           <input v-model="amountList" type="hidden" name="preferred_task" />
         </div>
         <div class="text">When do you want to meet up for your tree<span v-if="numberOfTrees > 1">s</span>? <span class="star">*</span></div>

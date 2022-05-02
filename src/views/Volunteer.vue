49c4d85727645a59ccdb65b006a43cf8ef623567
Coco Liliace <coco@liliace.dev>
2022-05-01 22:40:34 -0400

    fix: volunteer form and tree api

--- src/views/Volunteer.vue
+++ src/views/Volunteer.vue
@@ -50,7 +50,7 @@
       <div class="text">Any questions or comments? (Any medical concerns?)</div>
       <input v-model="comments" class="short-answer" type="text" name="comments" placeholder="Comments" autocomplete="off" />
       <div class="checkbox-container">
-        <label><input class="checkbox" type="checkbox" required /><span class="checkmark"></span>I have read and will follow the <router-link :to="{ name: 'Covid' }">Covid Volunteer Guidelines</router-link>. <span class="star" style="transform: translateX(-1em);">*</span></label>
+        <label><input class="checkbox" type="checkbox" required /><span class="checkmark radio"></span>I have read and will follow the <router-link :to="{ name: 'Covid' }">Covid Volunteer Guidelines</router-link>. <span class="star" style="transform: translateX(-1em);">*</span></label>
       </div>
       <button id="submit-form" class="primary-button" type="submit">Submit</button>
     </form>
@@ -124,7 +124,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.body {
+.contains-form {
   background-image: url("../assets/background.jpg");
   background-size: contain;
   background-attachment: fixed;
@@ -207,13 +207,13 @@ form {
         }
       }
     }
-    .checkmark {
+    .checkmark, .radio {
       position: absolute;
       margin-left: -40px;
       height: 18px;
       width: 18px;
       background-color: #eeeeee;
-      border-radius: 50%;
+      border-radius: 5px;
       box-shadow: inset 0 0 1px $blue;
       cursor: pointer;
       &:hover {
@@ -230,6 +230,10 @@ form {
         position: absolute;
         display: none;
       }
+    }
+    .radio {
+      border-radius: 50%;
+      box-shadow: inset 0 0 1px $blue;
     }
   }
   .row {
